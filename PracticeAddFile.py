884ee80e83393c761c48fa8c8a1a8b7cafa9f09f
DaveTheMovie <davethemovie@gmail.com>
2020-11-11 21:56:00 -0500

    Add anohter function

--- PracticeAddFile.py
+++ PracticeAddFile.py
@@ -2,3 +2,6 @@
 
 def PracticeAdd(a,b):
     return(a+b)
+
+def PracticeProd(a,b):
+    return(a*b)
--- test_PracticeAddFile.py
+++ test_PracticeAddFile.py
@@ -4,3 +4,4 @@ import PracticeAddFile
 
 def test_IAmTestingPracticeAdd():
     assert PracticeAddFile.PracticeAdd(1,2) == 3
+    assert PracticeAddFile.PracticeProd(1,2) == 2
